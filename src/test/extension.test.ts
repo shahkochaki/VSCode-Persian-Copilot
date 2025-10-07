929d12d25aa2a6c73202b217f3151ae096236da4
Ali Shahkochaki <ali.shahkochaki7@gmail.com>
2025-10-07 16:51:35 +0330

    Testing
    Test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -2,7 +2,6 @@ import * as assert from "assert";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-//testing note: the extension must be activated before running these tests
 import * as vscode from "vscode";
 // import * as myExtension from '../../extension';
 
